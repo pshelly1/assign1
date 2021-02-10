65f926addcdb086ca89cde153e3bb0caaa2c41e2
pshelly1 <68352985+pshelly1@users.noreply.github.com>
2021-02-10 04:01:51 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,49 @@ public class Main
 
         // TODO: F to C conversion
         //  Ask user for temp in F. Read it in. Convert to C. Display output.
+        float temperature;
         System.out.println("Let's convert temperature!");
         System.out.print("What's the temperature in Fahrenheit? ");
+        float C = (F - 32) * (9f / 5);
+       
+        
 
         // TODO: compute height of building
         //  Assume 20ft away. Ask how tall. Ask for angle of elevation.
         //  Compute height of building using Math.tan(). Display output.
+        import java.lang.Math;
+         public class
+              double buildingHeight;
+              double angleElevation;
+        
+           return Math.toRadians();
+        
+        angleElevation=0.4699;//0.4699 radians =0.00814;
+            buildingHeight=50;
+         
         System.out.println("Let's roughly compute the height of a building.");
         System.out.println("Assume you are standing 20ft away.");
+        System.out.print("What is the building height in meters?");
+        System.out.print("What is the building height in feet?");
+        System.out.print("Lookimg at the top, what is the angle of elevation?");
         System.out.print("How tall are you (ft)? ");
+       
 
         // TODO: compute probability of coveted loot.
         //  Ask for drop rate as percentage. Convert to fraction (divide by 100).
         //  Ask how many runs. Compute probability w/ formula provided.
+        import java.util;
+        
+        public class
+            
         System.out.println("Let's compute probability of dropped loot.");
+        System.out.print("What is the number of attempts?");
         System.out.print("What is the drop rate (%) of the loot you want? ");
+        
+        static void decimalToFraction*(double number);
+        static float bionomialProbability(int n, int k, float p)
+                       return (n, k) * (float)Math.pow(p, k)*
+                                    (float) MAth.pow(1 - p, n-k);
+        
     }
 }
